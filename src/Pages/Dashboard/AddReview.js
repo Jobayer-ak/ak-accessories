96234de7421bc8df7daec36e3a2100264b383a79
Jobayer-ak <jobayer.ak@gmail.com>
2022-05-29 01:01:03 +0600

    review added is done

--- src/Pages/Dashboard/AddReview.js
+++ src/Pages/Dashboard/AddReview.js
@@ -1,20 +1,65 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 
 const AddReview = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    console.log(data.rating);
+
+    if (data.rating < 1 && data.rating > 5) {
+      alert("Your review should be within 1 to 5!");
+    }
+
+    const review = {
+      feedback: data.review,
+      ratings: data.rating,
+    };
+
+    const url = "http://localhost:5000/reviews";
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(review),
+    })
+      .then((res) => res.json())
+      .then((inserted) => {
+        console.log("Reviews: ", inserted);
+        if (inserted.insertedId) {
+          toast.success("Review Added Successfully!");
+          reset();
+        } else {
+          toast.error("Failed To Add The Review!");
+        }
+      });
+  };
 
   return (
     <div className="text-center">
       <h2 className="text-3xl">Add A Review</h2>
-      <form onSubmit={handleSubmit(onSubmit)} className="text-center my-5">
-        <input {...register("firstName", { required: true, maxLength: 20 })} />
-        <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-        <input type="number" {...register("age", { min: 18, max: 99 })} />
-        <br />
-        <input type="submit" />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col justify-center items-center gap-5 my-5">
+        <textarea
+          className="w-2/4 p-3"
+          placeholder="Write your review"
+          rows="4"
+          cols="50"
+          {...register("review", { required: true, maxLength: 20 })}
+        />
+
+        {/* <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} /> */}
+        <input
+          className="w-1/4 p-3"
+          type="number"
+          placeholder="Give Us Ratings"
+          {...register("rating", { min: 1, max: 5 })}
+        />
+        <p>Your Rating should be within 1 to 5!</p>
+        <input className=" w-1/4 btn bnt-primary" type="submit" />
       </form>
     </div>
   );
