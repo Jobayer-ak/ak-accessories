221ac9c6c9d2df5edaa6b4ac313f190285298603
Jobayer-ak <jobayer.ak@gmail.com>
2022-05-31 00:01:07 +0600

    done

--- src/Pages/Home/Parts.js
+++ src/Pages/Home/Parts.js
@@ -25,7 +25,7 @@ const Parts = () => {
               .map((part) => <Part key={part._id} part={part}></Part>)
           : parts.map((part) => <Part key={part._id} part={part}></Part>)}
       </div>
-      <Footer></Footer>
+      
     </div>
   );
 };
